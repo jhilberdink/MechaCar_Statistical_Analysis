0be638b01c58270b5011094785f4954fbefe1e16
Jason Hilberdink <jhilberdink@gmail.com>
2021-06-05 12:13:15 -0400

    completed T-Tests of PSI

--- MechaCar_Challenge.R
+++ MechaCar_Challenge.R
@@ -16,3 +16,12 @@ suspension <- read.csv(file='Suspension_Coil.csv',check.names = F,stringsAsFacto
 total_summary <- suspension %>% summarize(mean=mean(PSI),median=median(PSI),variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% 
   summarize(mean=mean(PSI),median=median(PSI),variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+# Deliverable 3
+
+#perform T-Test for all lots against mean of 1500 PSI
+t.test(suspension$PSI,mu=1500)
+#perform T-Test for each individual lot
+t.test(subset(suspension, Manufacturing_Lot=='Lot1')$PSI,mu=1500)
+t.test(subset(suspension, Manufacturing_Lot=='Lot2')$PSI,mu=1500)
+t.test(subset(suspension, Manufacturing_Lot=='Lot3')$PSI,mu=1500)

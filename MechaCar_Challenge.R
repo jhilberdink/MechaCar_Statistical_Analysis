8886791a18a6e35b3d3a89df9da37e41428462e9
Jason Hilberdink <jhilberdink@gmail.com>
2021-06-05 11:32:43 -0400

    created multiple linear regression

---
+++ MechaCar_Challenge.R
@@ -0,0 +1,15 @@
+#Deliverable 1: Perform multiple linear regression to predict mpg
+
+#import tidyverse
+library(tidyverse)
+#import csv file
+mechacar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+# Generate multiple linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)
+# Generate summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg))
+
+# Deliverable 2
+
+#import csv file
+suspension <- read.csv(file='Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
